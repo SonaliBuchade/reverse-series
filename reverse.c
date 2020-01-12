711139ea06a5795450a6ce29e392cd2e9b0ba961
SonaliBuchade <59794298+SonaliBuchade@users.noreply.github.com>
2020-01-12 18:03:47 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a[9]={34,56,26,89,7,5,76,43,11};
+int a[10]={5,34,56,26,89,7,5,76,43,11};
 int i;
-for(i=0;i<9;i++)
+for(i=0;i<10;i++)
     {
         printf("%d ",a[i]);
     }
     printf("\n");
-    for(i=8;i>=0;i--)
+    for(i=9;i>=0;i--)
     {
         printf("%d ",a[i]);
     }
