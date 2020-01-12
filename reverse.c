9a28df443f1df3232e0f39305109ed773127df41
SonaliBuchade <59794298+SonaliBuchade@users.noreply.github.com>
2020-01-12 18:20:46 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+# define N 10
 int main()
 {
-int a[10]={5,34,56,26,89,7,5,76,43,11};
+int a[N]={5,34,56,26,89,7,5,76,43,11};
 int i;
-for(i=0;i<10;i++)
+for(i=0;i<N;i++)
     {
         printf("%d ",a[i]);
     }
     printf("\n");
-    for(i=9;i>=0;i--)
+    for(i=N-1;i>=0;i--)
     {
         printf("%d ",a[i]);
     }
